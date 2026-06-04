a471ad4154ade61ff11c4d6d14274e05873b28bd
Borno09890 <tashinirtijab@gmail.com>
2026-06-04 21:26:02 +0600

    modified:   configuration.nix 	modified:   flake.nix

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,7 @@
 }: {
   imports = [
     ./hardware-configuration.nix
+    ./flatpak.nix
   ];
   boot.loader.grub = {
     enable = true;
--- flake.nix
+++ flake.nix
@@ -15,9 +15,8 @@
     nixpkgs,
     home-manager,
     nix-flatpak,
-    inputs,
     ...
-  }: let
+  } @ inputs: let
     system = "x86_64-linux";
   in {
     nixosConfigurations.nix = nixpkgs.lib.nixosSystem {
